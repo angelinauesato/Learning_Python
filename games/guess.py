14b4e55990cf7112f245aa533e859b3cf7ccbbbe
Angelina Uesato <angelina.uesato@gmail.com>
2022-10-21 22:45:32 -0600

    if and else Statement
    Checking if the guessed number is bigger or not than the secret number

--- games/guess.py
+++ games/guess.py
@@ -1,14 +1,23 @@
 # Class Starting with Python from www.alura.com.br
+# Python 3
 print("**************************")
 print("Welcome to the Guess Game!")
 print("**************************")
 
 secret_number = 42
-guess = input("Guess a number: ")
+guess_str = input("Guess a number: ")
 
-print("You entered: ", guess)
+print("You entered: ", guess_str)
+guess = int(guess_str)
 
-if (secret_number == int(guess)):
+got_right = secret_number == guess
+bigger_guessed = secret_number < guess
+
+if got_right:
     print("You got the right number!")
 else:
-    print("You got wrong!")
+    if bigger_guessed:
+        print("You got wrong! The secret number is less than guessed number!")
+    else:
+        print("You got wrong! The secret number is bigger than guessed number!")
+print("End of the game.")
