60daaa661cb1f20678c045231809187d26d55208
Angelina Uesato <angelina.uesato@gmail.com>
2022-10-21 23:50:01 -0600

    For, break, continue and range
    Using For statement, continue, break and range function.
    - break stops the game when it's right
    - continue jumps to the next try when the number is not in between 1 and 100
    - range validates the quantity of try.

--- games/guess.py
+++ games/guess.py
@@ -6,23 +6,26 @@ print("**************************")
 
 secret_number = 42
 total_try = 3
-tries = 0
 got_right = False
-while tries < total_try and (not got_right):
-    tries += 1
+for tries in range(1, total_try+1): # range(start, stop, [step])
     # String Interpolation
     # print("Round: {} of {}".format(tries, total_try))
     print(f"Round: {tries} of {total_try}")
-    guess_str = input("Guess a number: ")
+    guess_str = input("Guess a number between 1 and 100: ")
+    guess = int(guess_str)
+
+    if guess < 1 or guess > 100:
+        print("Please, You have to enter a number between 1 and 100!")
+        continue
 
     print("You entered: ", guess_str)
-    guess = int(guess_str)
 
     got_right = secret_number == guess
     bigger_guessed = secret_number < guess
 
     if got_right:
         print("You got the right number!")
+        break
     else:
         if bigger_guessed:
             print("You got wrong! The secret number is less than guessed number!")
