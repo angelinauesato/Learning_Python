20c65784eedf3a9f000127ddee771cfa6cad7ffc
Angelina Uesato <angelina.uesato@gmail.com>
2022-10-21 23:06:39 -0600

    While Statement + String Interpolation
    The game will have tries even if the player gets right on the first try.

--- games/guess.py
+++ games/guess.py
@@ -5,19 +5,26 @@ print("Welcome to the Guess Game!")
 print("**************************")
 
 secret_number = 42
-guess_str = input("Guess a number: ")
+total_try = 3
+tries = 0
+while tries < total_try:
+    tries += 1
+    # String Interpolation
+    # print("Round: {} of {}".format(tries, total_try))
+    print(f"Round: {tries} of {total_try}")
+    guess_str = input("Guess a number: ")
 
-print("You entered: ", guess_str)
-guess = int(guess_str)
+    print("You entered: ", guess_str)
+    guess = int(guess_str)
 
-got_right = secret_number == guess
-bigger_guessed = secret_number < guess
+    got_right = secret_number == guess
+    bigger_guessed = secret_number < guess
 
-if got_right:
-    print("You got the right number!")
-else:
-    if bigger_guessed:
-        print("You got wrong! The secret number is less than guessed number!")
+    if got_right:
+        print("You got the right number!")
     else:
-        print("You got wrong! The secret number is bigger than guessed number!")
+        if bigger_guessed:
+            print("You got wrong! The secret number is less than guessed number!")
+        else:
+            print("You got wrong! The secret number is bigger than guessed number!")
 print("End of the game.")
